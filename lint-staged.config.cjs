fd43ab00812e6c635fd9510d1d138f1ae56a0c74
tal-rofe <talrofe111+1@gmail.com>
2024-02-29 11:14:51 +0200

    ci: 🚀 add Terraform validation in integration CI
    add Terraform validation in integration CI

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,6 @@
 const lintStagedConfig = {
 	'**/*.{ts,js,json,yaml}': 'prettier --write',
+	'**/*.tf': 'terraform fmt',
 };
 
 module.exports = lintStagedConfig;
--- terraform/core/route53.tf
+++ terraform/core/route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "primary" {
-  name = var.domain_name
+  name          = var.domain_name
   force_destroy = true
 
   tags = merge(
