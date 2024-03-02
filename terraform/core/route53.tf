9981deebf640684a12183de069b914a00d4c57b1
tal-rofe <talrofe111+1@gmail.com>
2024-03-02 11:34:13 +0200

    ci: 🚀 add comment for hosted zone
    add comment for hosted zone

--- terraform/core/route53.tf
+++ terraform/core/route53.tf
@@ -1,5 +1,6 @@
 resource "aws_route53_zone" "primary" {
   name          = var.domain_name
+  comment       = "Hosted Zone for URL shortener application, managed by Terraform"
   force_destroy = true
 
   tags = merge(
