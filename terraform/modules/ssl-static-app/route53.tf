a7e44f174d498b16da33a23b407703a973c23efa
tal-rofe <talrofe111+1@gmail.com>
2024-03-02 12:58:55 +0200

    refactor: 💡 add validation for web application certificate
    add validation for web application certificate

--- terraform/modules/ssl-static-app/route53.tf
+++ terraform/modules/ssl-static-app/route53.tf
@@ -21,3 +21,25 @@ resource "aws_route53_record" "redirect_route" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "acm_validation_record" {
+  for_each = {
+    for dvo in module.acm_cloudfront.acm_certificate_domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.zone_id
+}
+
+resource "aws_acm_certificate_validation" "acm_validation" {
+  certificate_arn         = module.acm_cloudfront.acm_certificate_arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_validation_record : record.fqdn]
+}
