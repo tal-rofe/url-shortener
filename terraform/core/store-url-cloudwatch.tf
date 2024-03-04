8e2c7af673cd0a63e6890bb4345754c93f2f7083
tal-rofe <talrofe111+1@gmail.com>
2024-03-04 22:52:30 +0200

    refactor: 💡 change policy resource
    change policy resource

--- terraform/core/store-url-cloudwatch.tf
+++ terraform/core/store-url-cloudwatch.tf
@@ -15,11 +15,33 @@ resource "aws_cloudwatch_log_group" "store_url_cloudwatch_log_group" {
   )
 }
 
-data "aws_iam_policy" "lambda_basic_execution_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_policy" "store_url_logging_policy" {
+  name = "Store-url-logging-policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        Action : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect : "Allow",
+        Resource : "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name  = "${var.project}-Store-URL-Logging-Policy"
+      Stack = "Backend"
+    }
+  )
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_flow_log_cloudwatch" {
-  role       = aws_iam_role.iam_for_lambda_store_url.name
-  policy_arn = data.aws_iam_policy.lambda_basic_execution_policy.arn
+  role       = aws_iam_role.iam_for_lambda_store_url.id
+  policy_arn = aws_iam_policy.store_url_logging_policy.arn
 }
