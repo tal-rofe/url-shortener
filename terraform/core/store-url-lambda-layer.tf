3092f5d0612720b0b97e4d2c4cf5eae348636ca1
tal-rofe <talrofe111+1@gmail.com>
2024-03-12 14:02:13 +0200

    refactor: 💡 separate actions and fix reference for data
    separate actions and fix reference for data

--- terraform/core/store-url-lambda-layer.tf
+++ terraform/core/store-url-lambda-layer.tf
@@ -24,6 +24,6 @@ resource "aws_lambda_layer_version" "store_url_lambda_layer" {
   description         = "This layer contains node modules required for the main lambda function to run"
   s3_bucket           = module.s3_store_url_lambda_layer_bucket.s3_bucket_id
   s3_key              = aws_s3_object.store_url_lambda_layer_s3_object.key
-  source_code_hash    = data.archive_file.main.output_base64sha256
+  source_code_hash    = data.archive_file.lambda_layer_store_url_zip.output_base64sha256
   compatible_runtimes = ["nodejs20.x"]
 }
